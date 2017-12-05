ed6c4efa0793e3fccc631634e60096d62f0f179a
yvesandre-middleton <yvesmiddleton@gmail.com>
2017-12-05 22:46:05 +0000

    Adds arrays exercises

---
+++ ice-cream-flavours.js
@@ -0,0 +1,11 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("Root beer");
+
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[5]);
+
+console.log(iceCreamFlavours.length);
+
