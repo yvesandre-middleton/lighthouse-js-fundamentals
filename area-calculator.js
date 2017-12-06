347d9838fd06dc1abd8423ab1c25069b44f04e4a
yvesandre-middleton <yvesmiddleton@gmail.com>
2017-12-06 17:28:55 +0000

    Adds looping exercises

---
+++ area-calculator.js
@@ -0,0 +1,9 @@
+function calculateRectangleArea(length, width){
+  return length * width
+}
+
+function calculateTriangleArea(base, height){
+  return base * height/2
+}
+
+console.log(calculateTriangleArea(10, 5));
